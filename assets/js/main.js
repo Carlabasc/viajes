7b06c93a296d81fb5a5d32e572968cad81a6a6cb
Carla <ca.m.bascunan@gmail.com>
2022-12-18 21:25:52 -0300

    primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  
+    $("#enviarcorreo").click(function () {
+      alert("El correo fue enviado correctamente...");
+    });
+
+})
+
+
+$(function(){
+$('.carousel').carousel({
+    interval: 2000
+  });
+
+
+$('#myCarousel').on('slide.bs.carousel', function () {
+  // do something…
+})
+
+})
+
+
+window.scrollTo({ top: 900, behavior: 'smooth' })
