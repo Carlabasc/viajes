dedc5f5a79ad175f3f219cdb073b71ccdb708769
Carla <ca.m.bascunan@gmail.com>
2022-12-20 16:39:03 -0300

    centre los parrafos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+// tooltip en boton de enviar
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 
@@ -6,10 +14,12 @@ $(function () {
   });
 });
 
-$(function () {
-  $(".carousel").carousel({
-    interval: 2000,
+//smooth scroll
+
+$(document).ready(function () {
+  $("a").click(function () {
+    var gato = this.hash;
+
+    $("html, body").animate({ scrolltop: $(gato).offset().top - 30 }, 3000);
   });
 });
-
-window.scrollTo({ top: 900, behavior: "smooth" });
